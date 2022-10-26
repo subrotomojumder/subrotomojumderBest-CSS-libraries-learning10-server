8e50e66d58935783c1f6d97b604f9e1bdc30b8ac
Janardhan Majumder <subrotomojumder03@gmail.com>
2022-10-26 14:39:08 +0600

    select specific course by id

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const libraries = require('./data/courses.json')
 app.get('/courses', (req, res)=> {
     res.send(libraries)
 })
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = libraries.find(course => course.id === id)
+    res.send(selectedCourse)
+})
 
 app.listen(port, ()=> {
     console.log('this is port', port)
